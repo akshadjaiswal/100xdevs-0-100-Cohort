8d5d07693d53e555388be0e36579baf49192e593
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-07-15 15:50:07 +0530

    easy assignment problem solve

--- Coding/Week 01 - Javascript/Assignments/01-js/easy/findLargestElement.js
+++ Coding/Week 01 - Javascript/Assignments/01-js/easy/findLargestElement.js
@@ -6,7 +6,13 @@
 */
 
 function findLargestElement(numbers) {
-    
+  let largeElement = numbers[0];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > largeElement) {
+      largeElement = numbers[i];
+    }
+  }
+  return largeElement;
 }
 
 module.exports = findLargestElement;
