243bc7a5f1b6c3423c39e04dafca7bf714ebce65
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-07-18 14:36:27 +0530

    complete 1.3 + basics

--- Coding/Week 01 - Javascript/Code/02 Basic JS API's/level-2/01-Class.js
+++ Coding/Week 01 - Javascript/Code/02 Basic JS API's/level-2/01-Class.js
@@ -1,9 +1,31 @@
 class Animal {
-  constructor(name, legCount) {
+  constructor(name, legCount, speaks) {
     this.name = name;
     this.legCount = legCount;
+    this.speaks = speaks;
   }
-  describe() {
-    return `${this.name} has ${this.legCount} legs`;
+  static myType() {
+    console.log("Static");
+  }
+  animalName() {
+    return this.name;
+  }
+  speak() {
+    return this.speaks;
+  }
+  legs() {
+    return this.legCount;
   }
 }
+console.log(Animal.myType());
+let dog = new Animal("Bruno", 4, "Bhow Bhow");
+let cat = new Animal("Mini", 4, "Meow Meow");
+console.log(
+  dog.animalName() +
+    ": " +
+    dog.speak() +
+    " " +
+    cat.animalName() +
+    ": " +
+    cat.speak()
+);
