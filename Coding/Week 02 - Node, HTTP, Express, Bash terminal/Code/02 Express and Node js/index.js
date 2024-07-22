2202235fde0dc79abe420ad0fec1fb7f9db140e8
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-07-22 20:49:15 +0530

    Bash Important commands

--- Coding/Week 02 - Node, HTTP, Express, Bash terminal/Code/02 Express and Node js/index.js
+++ Coding/Week 02 - Node, HTTP, Express, Bash terminal/Code/02 Express and Node js/index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const port = process.env.PORT ||3000
+const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
-app.get('/home',(req , res)=>{
-  res.send("Akshad")
-})
-app.get('/',(req , res)=>{
-  res.send("Main Page")
-})
+app.get("/home", (req, res) => {
+  res.send("Akshad");
+});
+app.get("/", (req, res) => {
+  res.send("Main Page");
+});
 app.post("/api", (req, res) => {
   console.log(req.query.message);
   res.send("Hello World");
