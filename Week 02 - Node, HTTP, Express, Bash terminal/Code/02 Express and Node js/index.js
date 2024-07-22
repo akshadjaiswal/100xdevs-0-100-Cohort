e0c59cfe945fb73a233adb95f9ef4f749ba93e90
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-07-22 12:49:58 +0530

    Post request

--- Week 02 - Node, HTTP, Express, Bash terminal/Code/02 Express and Node js/index.js
+++ Week 02 - Node, HTTP, Express, Bash terminal/Code/02 Express and Node js/index.js
@@ -3,7 +3,13 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send('<b>Hello World</b>');
+  // res.send("Hello World");
+});
+
+app.post("/conversation", (req, res) => {
+  res.send('<b>Conversation </b>');
+  // res.send("Hello World");
 });
 
 app.listen(port, () => {
