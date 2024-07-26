4b309f5550244bb85a21ab03b2829d5fe761a076
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-07-26 19:54:23 +0530

    formatting

--- Coding/Week 02 - Node, HTTP, Express, Bash terminal/Code/06 Flter, map and arrow fns/functions.js
+++ Coding/Week 02 - Node, HTTP, Express, Bash terminal/Code/06 Flter, map and arrow fns/functions.js
@@ -4,7 +4,7 @@ function multiply(a, b) {
 const answer = multiply(2, 3);
 console.log(answer);
 
-const multiply2=(a,b)=>{
-    return a*b;
-}
-console.log(multiply2(2,3))
+const multiply2 = (a, b) => {
+  return a * b;
+};
+console.log(multiply2(2, 3));
