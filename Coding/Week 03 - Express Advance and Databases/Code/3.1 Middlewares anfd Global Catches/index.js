a56b7c5dfc666a46a08728d3e449543ac766f766
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-07-30 16:08:55 +0530

    Adding middlewares

--- Coding/Week 03 - Express Advance and Databases/Code/3.1 Middlewares anfd Global Catches/index.js
+++ Coding/Week 03 - Express Advance and Databases/Code/3.1 Middlewares anfd Global Catches/index.js
@@ -2,8 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/health-checkup", (req, res) => {
-  const kidneyId = req.query.kidneyId;
+function userMiddleware(req, res, next) {
   const username = req.headers.username;
   const password = req.headers.password;
 
@@ -11,34 +10,26 @@ app.get("/health-checkup", (req, res) => {
     res.status(403).json({
       msg: "User invalid",
     });
-    return;
+  } else {
+    next();
   }
+}
+
+function kidneyMiddleware(req, res, next) {
+  const kidneyId = req.query.kidneyId;
   if (kidneyId != 1 && kidneyId != 2) {
     res.status(411).json({
       msg: "Wrong inputs",
     });
-    return;
+  } else {
+    next();
   }
+}
+app.get("/health-checkup", userMiddleware, kidneyMiddleware, (req, res) => {
   res.send("Your are healthy");
 });
 
-app.put("/replace-kidney", (req, res) => {
-  const kidneyId = req.query.kidneyId;
-  const username = req.headers.username;
-  const password = req.headers.password;
-
-  if (username != "Akshad" || password != "999") {
-    res.status(403).json({
-      msg: "User invalid",
-    });
-    return;
-  }
-  if (kidneyId != 1 && kidneyId != 2) {
-    res.status(411).json({
-      msg: "Wrong inputs",
-    });
-    return;
-  }
+app.put("/replace-kidney", kidneyMiddleware, (req, res) => {
   res.send("Healthy");
 });
 
