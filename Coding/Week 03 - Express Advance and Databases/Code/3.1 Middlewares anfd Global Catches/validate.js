8e2146a830ff31571495857a1500fc94425f8cf1
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-08-02 19:18:47 +0530

    Zod implementation in HTTP server

--- Coding/Week 03 - Express Advance and Databases/Code/3.1 Middlewares anfd Global Catches/validate.js
+++ Coding/Week 03 - Express Advance and Databases/Code/3.1 Middlewares anfd Global Catches/validate.js
@@ -1,9 +1,27 @@
 //if this is an array of number with aleats 1 input , trturn true else false
+const express = require("express");
+const app = express();
+app.use(express.json());
+const port = 3000;
 const zod = require("zod");
-function validateInput(arr) {
-  const schema = zod.array(zod.number());
-  const response = schema.safeParse(arr);
-  console.log(response)
+function validateInput(obj) {
+  const schema = zod.object({
+    email: zod.string().email(),
+    password: zod.string().min(8),
+  });
+  const response = schema.safeParse(obj);
+  console.log(response);
 }
-// console.log(validateInput(["Akshad"]));
-validateInput([1,2,3])
+// validateInput({
+//   email: "akshadsantoshjaiswal@gmail.com",
+//   password: "12345678",
+// });
+app.post("/signUp", (req, res) => {
+  const response1 = validateInput(req.body);
+  res.json({
+    msg: "Valid Inputs",
+  });
+});
+app.listen(port, () => {
+  console.log("App is starting on port " + port);
+});
