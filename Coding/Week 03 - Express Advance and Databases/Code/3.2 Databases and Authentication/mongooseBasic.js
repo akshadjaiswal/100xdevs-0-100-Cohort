280ac4a377cd20a020185ce028131dbface4bc5d
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-08-09 13:12:39 +0530

    Inserting data into database from backend

--- Coding/Week 03 - Express Advance and Databases/Code/3.2 Databases and Authentication/mongooseBasic.js
+++ Coding/Week 03 - Express Advance and Databases/Code/3.2 Databases and Authentication/mongooseBasic.js
@@ -1,11 +1,45 @@
+const express = require("express");
 const mongoose = require("mongoose");
 mongoose.connect(
   "mongodb+srv://akshadsantoshjaiswal:rFw7NSxncTpSc7Bq@cluster0.yulixmn.mongodb.net/users_app?"
 );
 
-const User = mongoose.model("User", { name: String , email:String , password:String });
+const User = mongoose.model("User", {
+  name: String,
+  email: String,
+  password: String,
+});
 
-const user = new User({ name: "Akshad", email:"akshad@gmail.com" ,password:"999" });
-const user2 = new User({ name: "Aaradhana", email:"aaradhana@gmail.com" ,password:"032002" });
-user.save().then(()=>{console.log("Successfully inserted into database")})
-user2.save().then(()=>{console.log("Successfully inserted into database")})
+const app = express();
+app.use(express.json());
+app.post("/signup", async function (req, res) {
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const existingUser = await User.findOne({ email: email });
+  if (existingUser) {
+    return res.status(400).send("User Already Exist");
+  }
+
+  const user = new User({
+    name: name,
+    email: email,
+    password: password,
+  });
+
+  user.save().then(()=>{
+    console.log("Inserted ")
+  });
+  
+  res.json({
+    msg: "User created ",
+  });
+});
+app.get("/",(req,res)=>{
+    res.send("Working")
+})
+
+app.listen(3000, () => {
+  console.log("App running on port 3000");
+});
