3226c2bc5c51ad7cc9baa389b66516012e2f928d
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-08-14 22:29:06 +0530

    Assignment

--- 0-1/Coding/Week 04 - Frontend Foundations/Code/4.1 DOM Introduction/Script.js
+++ 0-1/Coding/Week 04 - Frontend Foundations/Code/4.1 DOM Introduction/Script.js
@@ -1,6 +1,11 @@
-function populateDiv() {
+async function populateDiv() {
   const a = document.getElementById("firstNumber").value;
   const b = document.getElementById("secondNumber").value;
-  const element = document.getElementById("finalSum");
-  element.innerHTML =  parseInt(a) + parseInt(b);
+  //   const element = document.getElementById(/"finalSum");
+  const response = await fetch(
+    "https://sum-server.100xdevs.com/sum?a=" + a + "&b=" + b
+  );
+  const ans = await response.text();
+  document.getElementById("finalSum").innerHTML = ans;
+  console.log(ans);
 }
