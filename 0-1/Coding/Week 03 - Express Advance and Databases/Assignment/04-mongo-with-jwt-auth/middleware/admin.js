2ea48a00820f7c67858502d97e10496c44f10c87
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-09-10 14:37:47 +0530

    Assignment:Mongo With JWT Authentication done

--- 0-1/Coding/Week 03 - Express Advance and Databases/Assignment/04-mongo-with-jwt-auth/middleware/admin.js
+++ 0-1/Coding/Week 03 - Express Advance and Databases/Assignment/04-mongo-with-jwt-auth/middleware/admin.js
@@ -1,7 +1,23 @@
 // Middleware for handling auth
+const jwt = require("jsonwebtoken")
+const secret = require("../index");
+const { JWT_SECRET } = require("../Config");
 function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+    //authorization= Bearer "jhdcjwebfij"
+    const token = req.headers.authorization;
+    const words = token.split(" ");
+    const jwtToken = words[1];
+    const decodedValue = jwt.verify(jwtToken, JWT_SECRET)
+    if (decodedValue.username) {
+        next()
+    } else {
+        res.status(403).json({
+            msg: "You are not Authenticated"
+        })
+    }
+
 }
 
 module.exports = adminMiddleware;
