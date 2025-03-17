1e42ee6cb149c42fca83f8a6c1ebb0cf9c72c95e
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2025-03-18 01:29:49 +0530

    Todo challenge

--- 0-1/Coding/Week 06 - More of React/react-hooks/src/App.jsx
+++ 0-1/Coding/Week 06 - More of React/react-hooks/src/App.jsx
@@ -1,22 +1,50 @@
 import React, { useState } from "react";
 import Header from "./Header";
+import Todo from "./Todo";
 // import HeaderWithButton from "./HeaderWithButton";
 
 function App() {
-  const [title, setTitle] = useState("Akshad");
+  // const [title, setTitle] = useState("Akshad");
+  const [todos, setTodos] = useState([
+    {
+      title: "Gym",
+      description: "At 6 am",
+    },
+    {
+      title: "Code",
+      description: "At 7 am",
+    },
+  ]);
 
-  const updateTitle = () => {
-    let newTitle = "My name is " + Math.random();
-    setTitle(newTitle);
+  // const updateTitle = () => {
+  //   let newTitle = "My name is " + Math.random();
+  //   setTitle(newTitle);
+  // };
+  const addTodo = () => {
+    setTodos([
+      ...todos,
+      {
+        title: Math.random(),
+        description: Math.random(),
+      },
+    ]);
   };
   return (
     <div>
       {/* <HeaderWithButton></HeaderWithButton> */}
-      <button onClick={updateTitle}>Click to update Title</button>
+      {/* <button onClick={updateTitle}>Click to update Title</button>
       <Header title={title} />
       <Header title="Akshad jjj" />
       <Header title="Akshad jjj" />
-      <Header title="Akshad jjj" />
+      <Header title="Akshad jjj" /> */}
+      {todos.map((todo, index) => {
+        return (
+          <div key={index}>
+            <Todo title={todo.title} description={todo.description}></Todo>
+          </div>
+        );
+      })}
+      <button onClick={addTodo}>Add Todo</button>
     </div>
   );
 }
