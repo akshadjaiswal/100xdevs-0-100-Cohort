733e0e334fd2ddea7a6a90910199e12fddb4d19b
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2025-03-19 23:38:54 +0530

    Wrapper components :heavy_plus_sign:

--- 0-1/Coding/Week 06 - More of React/react-hooks/src/App.jsx
+++ 0-1/Coding/Week 06 - More of React/react-hooks/src/App.jsx
@@ -1,58 +1,19 @@
-import React, { useState } from "react";
-import Header from "./Header";
-import Todo from "./Todo";
-// import HeaderWithButton from "./HeaderWithButton";
-
-let counter = 3;
+import TextComponent from "./TextComponent";
 
 function App() {
-  // const [title, setTitle] = useState("Akshad");
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      title: "Gym",
-      description: "At 6 am",
-    },
-    {
-      id: 2,
-      title: "Code",
-      description: "At 7 am",
-    },
-  ]);
-
-  // const updateTitle = () => {
-  //   let newTitle = "My name is " + Math.random();
-  //   setTitle(newTitle);
-  // };
-  const addTodo = () => {
-    setTodos([
-      ...todos,
-      {
-        id: counter++,
-        title: Math.random(),
-        description: Math.random(),
-      },
-    ]);
-    console.log(todos)
-  };
   return (
     <div>
-      {/* <HeaderWithButton></HeaderWithButton> */}
-      {/* <button onClick={updateTitle}>Click to update Title</button>
-      <Header title={title} />
-      <Header title="Akshad jjj" />
-      <Header title="Akshad jjj" />
-      <Header title="Akshad jjj" /> */}
-      {todos.map((todo) => {
-        return (
-          <div key={todo.id}>
-            <Todo title={todo.title} description={todo.description}></Todo>
-          </div>
-        );
-      })}
-      <button onClick={addTodo}>Add Todo</button>
+      <Cardwrapper children={<TextComponent title={"Akshad"} />} />
+      <Cardwrapper children={<TextComponent title={"Full Stack developer"} />} />
     </div>
   );
 }
 
+function Cardwrapper({ children }) {
+  return (
+    <div style={{ border: "2px solid black", padding: 20, margin: 20 }}>
+      {children}
+    </div>
+  );
+}
 export default App;
